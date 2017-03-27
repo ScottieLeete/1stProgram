cfac65fb32a67462e12b84e2cfa1714c0349e1d9
Jon Leete <jleete97@gmail.com>
2017-03-27 16:55:26 -0400

    second commit

--- f2c.c
+++ f2c.c
@@ -6,6 +6,7 @@ main()
 	int fahr, celsius;
 	int lower, upper, step;
 
+	/* initialize */
 	lower = -40; /* lower limit of temperature table */
 	upper = 300; /* upper limit */
 	step = 20; /* step size */
@@ -15,5 +16,6 @@ main()
 		celsius = 5 * (fahr-32) / 9;
 		printf("%d\t%d\n", fahr, celsius);
 		fahr = fahr + step;
+		/*Okay, we're done. Now what?*/
 	}
 }
